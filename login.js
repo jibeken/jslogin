a2dbd2a6998877d9f564e58d8534448e8c5a3955
jikuloveschicken <kenjibek1606@icloud.com>
2024-10-21 13:23:21 +0600

    login.js

--- login.js
+++ login.js
@@ -1,11 +1,8 @@
 let usernameInput = prompt("Enter username:");
 let passwordInput = prompt("Enter password:");
 function login(username, password) {
-    // Convert username to lowercase for case-insensitive comparison
     const correctUsername = "admin";
     const correctPassword = "aidai123";
-
-    // Convert both usernames to lowercase for case-insensitivity
     if (username.toLowerCase() === correctUsername) {
         if (password === correctPassword) {
             return "Login successful!";
@@ -16,6 +13,4 @@ function login(username, password) {
         return "User not found.";
     }
 }
-
-// Displaying the result in console
 alert(login(usernameInput, passwordInput));
